b6e0f825a6a7dfa0eb435f9d917d624d8cbc4ddf
imanol <jjusturi@gmail.com>
2022-05-12 16:39:42 -0500

    base_model.py:white_chek_mark:

--- models/base_model.py
+++ models/base_model.py
@@ -7,7 +7,7 @@ from datetime import datetime
 class BaseModel:
     """A base class for all hbnb models"""
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
+        """Instantiates a new model"""
         if not kwargs:
             from models import storage
             self.id = str(uuid.uuid4())
@@ -19,7 +19,7 @@ class BaseModel:
                                                      '%Y-%m-%dT%H:%M:%S.%f')
             kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
                                                      '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
+            #del kwargs['__class__']
             self.__dict__.update(kwargs)
 
     def __str__(self):
