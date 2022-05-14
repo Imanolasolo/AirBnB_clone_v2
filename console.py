36b007ec74236ae04bda1fd72d12ae531cd4b2bb
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 19:00:37 -0500

    Update Console.py | Fixing Style:heavy_check_mark:

--- console.py
+++ console.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+# File: console.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
+
 """ Console Module """
 import cmd
 import sys
-from datetime import datetime
 import re
 import os
 import uuid
+from datetime import datetime
 
 from models.base_model import BaseModel
 from models import storage
