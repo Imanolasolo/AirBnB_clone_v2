817a8a71999666953c8762be75bd7689c84efad3
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 19:17:17 -0500

    Update file | Styling | Authors ✔️

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,6 +1,11 @@
--- script that prepares a MySQL server for the project
+/*File: setup_mysql_test.sql
+Authors: Imanol Asolo - Alex Arévalo
+email(s): <3848@holbertonschool.com>        
+          <3915@holbertonschool.com>*/
+          
+-- Script that prepares a MySQL server for the project
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
-GRANT ALL ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
+GRANT ALL ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 FLUSH PRIVILEGES;
