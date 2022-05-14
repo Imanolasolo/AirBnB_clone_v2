5dc59f97432dc97085c6c92a5f09dc5a3e62faac
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 20:11:48 -0500

    Update file | Styling & Commit ✔️

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-""" """
+# File: test_base_model.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
+
+"""This Module Defines Unittest for models/base_model.py"""
 from models.base_model import BaseModel
 import unittest
 from datetime import datetime
