016088d7e162213df0fada32a1e84b1d8635a063
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 20:00:29 -0500

    Update file | Styling ✔️

--- models/user.py
+++ models/user.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
+# File: user.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
 """This module defines a class User"""
 from models.base_model import BaseModel
 
