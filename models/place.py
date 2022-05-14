e6e35dbbfd307a89fbe81acbe7a4278bf7c79ca1
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 19:55:16 -0500

    Update file | Styling ✔️

--- models/place.py
+++ models/place.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+# File: place.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
+
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
 
