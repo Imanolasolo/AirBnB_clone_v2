e15c1b360162649a46b8ce10eac7b8a6f5ea9773
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 20:16:57 -0500

    Update file | Styling | Commit ✔️

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-""" """
+# File: test_place.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
+
+"""This Module Defines Unittest for models/place.py"""
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
 import os
