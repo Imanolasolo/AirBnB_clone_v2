8ecc4012a8a534a21ff655df5c4e63e434c6b71e
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 20:18:31 -0500

    Update file | Styling | Commit ✔️

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
-""" """
+# File: test_review.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
+
+"""This Module Defines Unittest for models/review.py"""
 from tests.test_models.test_base_model import test_basemodel
 from models.review import Review
 import os
