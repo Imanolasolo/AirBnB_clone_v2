b9615fb5daeb044b3171b4660a24d2d14f786f68
imanol <jjusturi@gmail.com>
2022-05-14 11:36:39 -0500

    amenity.py:white_check_mark:

--- models/amenity.py
+++ models/amenity.py
@@ -6,10 +6,17 @@
 # Collaborators: Imanol Asolo - Alex Arévalo
 # email(s): <3848@holbertonschool.com>
 #           <3915@holbertonschool.com>
+""" Amenity Module for HBNB project """
+import os
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """Represents an amenity data set."""
+    __tablename__ = 'amenities'
+    name = Column(
+        String(128), nullable=False
+    ) if os.getenv('HBNB_TYPE_STORAGE') == 'db' else ''
