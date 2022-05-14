a0706700cee165167dc004cc9db2d8d40372936a
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 19:44:14 -0500

    Update File | Styling ✔️

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+# File: file_storage.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
+
 """This module defines a class to manage file storage for hbnb clone"""
 import json
 import os
