c4c4ac25913bb86a078e9305a216c42807826ede
Alex O. Arévalo T <91083806+Alexoat76@users.noreply.github.com>
2022-05-13 19:53:21 -0500

    Update file | Styling ✔️

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+# File: __init__.py
+# Main Authors: Justin Majetich - Ezra Nobrega
+# email(s): <justinmajetich@gmail.com>
+#           <ezra.nobrega@outlook.com>
+# Collaborators: Imanol Asolo - Alex Arévalo
+# email(s): <3848@holbertonschool.com>
+#           <3915@holbertonschool.com>
+
 """This module instantiates an object of class FileStorage"""
 from models.engine.file_storage import FileStorage
 
