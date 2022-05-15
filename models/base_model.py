5ed305047390cece8d70168de6f09bf131603db4
imanol <jjusturi@gmail.com>
2022-05-15 11:43:16 -0500

    base_model.py: white_check_mark:

--- models/base_model.py
+++ models/base_model.py
@@ -16,6 +16,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True, unique=True)
@@ -71,4 +72,3 @@ class BaseModel:
                     res[key] = value
         res['__class__'] = self.__class__.__name__
         return res
-
