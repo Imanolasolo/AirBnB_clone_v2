c7f1322a85fa30e9b2ca0c4a3e97c11e0a200f91
imanol <jjusturi@gmail.com>
2022-05-24 17:26:15 -0500

    __init__.py:wrench:

--- models/__init__.py
+++ models/__init__.py
@@ -9,7 +9,13 @@
 
 """This module is A unique FileStorage/DBStorage instance for all models"""
 import os
-
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models.engine.db_storage import DBStorage
 from models.engine.file_storage import FileStorage
 
