158d4ce29b1675c7aa0a1650cbcb830bccdd72d7
imanol <jjusturi@gmail.com>
2022-05-25 17:47:23 -0500

    7-states_list.py:wrench:

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,14 +1,14 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 # File: 7-states_list.py
-# Author: Imanol Asolo.
-
+# Author: Imanol Asolo
 
 """"
 Script starts Flask web app
-listen on 0.0.0.0, port 5000
+    listen on 0.0.0.0, port 5000
     routes: /:
             /states_list: Display HTML and state info from storage;
 """
+
 from models import storage
 from models.state import State
 from flask import Flask, render_template
@@ -26,7 +26,7 @@ def teardown_db(self):
 @app.route('/states_list')
 def states_list():
     """Displays an HTML page with a list of all State objects in DBStorage.
-    States are sorted by name.
+       States are sorted by name.
     """
     states = storage.all(State).values()
     return(render_template("7-states_list.html", states=states))
