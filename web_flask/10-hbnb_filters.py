5ec715cecdb3e702db5522fe27b419a6eacceeec
imanol <jjusturi@gmail.com>
2022-05-25 18:56:46 -0500

    10-hbnb_filters:wrench:

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,30 +1,34 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /hbnb_filters: HBnB HTML filters page.
-"""
+""" Flask example """
+from flask import Flask, render_template
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from models.amenity import Amenity
 
 app = Flask(__name__)
-
-
-@app.route("/hbnb_filters", strict_slashes=False)
-def hbnb_filters():
-    """Displays the main HBnB filters HTML page."""
-    states = storage.all("State")
-    amenities = storage.all("Amenity")
-    return render_template("10-hbnb_filters.html",
-                           states=states, amenities=amenities)
+storage.all()
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_data(self):
+    """ Data from source """
     storage.close()
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def filter(id=None):
+    """ /hbnb_filters path """
+    data = storage.all(State)
+    states = []
+    for statedata in data:
+        states.append(data[statedata])
+
+    data = storage.all(Amenity)
+    amenities = []
+    for amenitiedata in data:
+        amenities.append(data[amenitiedata])
+
+    return render_template('10-hbnb_filters.html', states=states,
+        amenities=amenities)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
