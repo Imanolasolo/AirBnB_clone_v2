1c13d82dcd4a48e47f6a4d72ca8066a95f2f72d4
imanol <jjusturi@gmail.com>
2022-05-25 18:34:08 -0500

    9-states.py:wrench:

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states: HTML page with a list of all State objects.
-    /states/<id>: HTML page displaying the given state with <id>.
+# File: 9-states.py
+# Author: Alex Orland Arévalo Tribaldos
+# email: <3915@holbertonschool.com>
+
+""""
+Script starts Flask web app
+    listen on 0.0.0.0, port 5000
+    routes: /:
+            /states/<id>:    Display HTML and state, city given state id
 """
+
 from models import storage
-from flask import Flask
-from flask import render_template
+from models.state import State
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-
-@app.route("/states", strict_slashes=False)
-def states():
-    """Displays an HTML page with a list of all States.
-    States are sorted by name.
-    """
-    states = storage.all("State")
-    return render_template("9-states.html", state=states)
-
-
-@app.route("/states/<id>", strict_slashes=False)
-def states_id(id):
-    """Displays an HTML page with info about <id>, if it exists."""
-    for state in storage.all("State").values():
-        if state.id == id:
-            return render_template("9-states.html", state=state)
-    return render_template("9-states.html")
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(self):
+    """After each request remove current SQLAlchemy session"""
     storage.close()
 
 
+@app.route('/states')
+@app.route('/states/<id>')
+def states_and_cities(id=None):
+    """Display html page; customize heading with state.name
+       fetch sorted cities for this state ID into LI tag ->in HTML file
+    """
+    states = storage.all(State).values()
+    if id is not None:
+        for state in states:
+            if state.id == id:
+                return(render_template("9-states.html", states=state))
+        return(render_template("9-states.html"))
+    return(render_template("9-states.html", states=states, variable=True))
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port='5000')
+    app.run(host="0.0.0.0", port=5000)
     
